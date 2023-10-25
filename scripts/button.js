c23099275567691f99a8087f585d2a9d40cf53db
maisam2004 <maisam_2004@hotmail.com>
2023-10-25 11:02:24 +0000

    Loading whole DOM to check h1

--- scripts/button.js
+++ scripts/button.js
@@ -2,4 +2,5 @@ function buttonClick() {
     document.getElementById("par").innerHTML = "You Clicked";
 }
 
+
 module.exports = buttonClick;
--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -4,13 +4,31 @@
 const { TestScheduler } = require('jest');
 const buttonClick = require('../button');
 
-beforeEach(()=>{
+/* beforeEach(()=>{
     document.body.innerHTML="<p id='par' ></p>";
 });
+ */
+//The second approach, then, is to load the  entire HTML page and attach it to the mock DOM. 
+//for test more than one element   our >> 
+
+beforeEach(()=>{
+    let fs = require('fs'); // << node file handling module -read,write,modify
+    let fileContents = fs.readFileSync('index.html','utf-8');
+    document.open();
+    document.write(fileContents);
+    document.close();
+})
+
 
 describe("Dom tests",()=>{
     test('expects p contents to change',()=>{
         buttonClick();
+
         expect(document.getElementById('par').innerHTML).toEqual("You Clicked");
     });
+    test('h1 element should exist ',() =>{
+        expect(document.getElementsByTagName('h1').length).toBe(1);
+
+        //expect(document.getElementsByTagName('h1')[0].innerHTML).toEqual('Jest Click Test');
+    })
 })
